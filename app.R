a3e10644c3498a4e0a1e1095569fa2fa0726fa2d
Renato Cava <renatocava@msn.com>
2024-11-20 01:25:06 -0500

    se limpio

--- app.R
+++ app.R
@@ -8,11 +8,6 @@ options(
   gargle_oauth_email = "renato.cava@upch.pe"
 )
 
-# penguins_csv <- "https://raw.githubusercontent.com/jcheng5/simplepenguins.R/main/penguins.csv"
-
-# df <- readr::read_csv(penguins_csv)
-# Find subset of columns that are suitable for scatter plot
-# df_num <- df |> select(where(is.numeric), -Year)
 
 ui <- page_sidebar(
   # App theme ----
